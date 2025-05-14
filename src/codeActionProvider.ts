aa4e263a37fd7360f32a73791f835688b4ea56f4
ocet247 <ocet247@gmail.com>
2025-05-14 15:32:14 +0200

    SetAngles -> SetAbsAngles QAngle(...) insertion

--- src/codeActionProvider.ts
+++ src/codeActionProvider.ts
@@ -4,7 +4,7 @@ import { CharCode, ForwardIterator } from "./textProcessing";
 
 
 export default class TF2VScriptCodeActionProvider implements CodeActionProvider {
-	private getParamRanges(document: TextDocument, position: Position): { deleteRange: Range, insertText: string } | undefined {
+	private getParamRange(document: TextDocument, position: Position): { deleteRange: Range, insertText: string } | undefined {
 		const iterator = new ForwardIterator(ForwardIterator.textFromPosition(document, position));
 		const offset = document.offsetAt(position);
 
@@ -102,6 +102,74 @@ export default class TF2VScriptCodeActionProvider implements CodeActionProvider 
 			insertText
 		}
 	}
+
+	private getCallBodyRange(document: TextDocument, position: Position): Range | undefined {
+		const iterator = new ForwardIterator(ForwardIterator.textFromPosition(document, position));
+		const offset = document.offsetAt(position);
+
+		while (true) {
+			if (!iterator.hasNext()) {
+				return;
+			}
+			const char = iterator.next();
+
+			if (char === CharCode.LEFT_ROUND) {
+				break;
+			} else if (!CharCode.isWhitespace(char)) {
+				return;
+			}
+		}
+		const startPos = document.positionAt(offset + iterator.getCursor());
+
+		let depth = 1;
+		while (iterator.hasNext()) {
+			let char = iterator.next();
+
+			switch (char) {
+			case CharCode.RIGHT_ROUND:
+			case CharCode.RIGHT_CURLY:
+			case CharCode.RIGHT_SQUARE:
+				depth--;
+				if (depth === 0) {
+					const endPos = document.positionAt(offset + iterator.getCursor() - 1);
+
+					return new Range(startPos, endPos);
+				}
+				break;
+			case CharCode.LEFT_CURLY:
+			case CharCode.LEFT_SQUARE:
+			case CharCode.LEFT_ROUND:
+				depth++;
+				break;
+			case CharCode.DOUBLE_QUOTE:
+			case CharCode.QUOTE:
+			case CharCode.BACKTICK:
+				const opening = char;
+				// find the closing quote
+				while (iterator.hasNext()) {
+					char = iterator.next();
+							
+					// Ignore escape chars
+					if (char === CharCode.BACKSLASH) {
+						if (!iterator.hasNext()) {
+							break;
+						}
+
+						iterator.next();
+					}
+
+					if (char === opening) {
+						break;
+					}
+				}
+			}
+		}
+
+		const endPos = document.positionAt(offset + iterator.getCursor());
+
+		return new Range(startPos, endPos);
+	}
+
 	public provideCodeActions(document: TextDocument, range: Range, context: CodeActionContext, token: CancellationToken): CodeAction[] {
 		const fixes: CodeAction[] = []
 		for (const diagnostic of context.diagnostics) {
@@ -117,7 +185,7 @@ export default class TF2VScriptCodeActionProvider implements CodeActionProvider 
 					continue;
 				}
 				
-				const ranges = this.getParamRanges(document, diagnostic.range.end);
+				const ranges = this.getParamRange(document, diagnostic.range.end);
 				
 				if (!ranges) {
 					continue;
@@ -143,7 +211,7 @@ export default class TF2VScriptCodeActionProvider implements CodeActionProvider 
 			if (!successor) {
 				continue;
 			}
-			
+
 			const fix = new CodeAction(
 				`Replace with '${successor}'`,
 				CodeActionKind.QuickFix
@@ -151,6 +219,17 @@ export default class TF2VScriptCodeActionProvider implements CodeActionProvider 
 
 			fix.edit = new WorkspaceEdit();
 			fix.edit.replace(document.uri, diagnostic.range, successor);
+
+			// Yes a hardcode
+			if (successor === "SetAbsAngles") {
+				const bodyRange = this.getCallBodyRange(document, diagnostic.range.end);
+				if (bodyRange) {
+					fix.edit.insert(document.uri, bodyRange.start, "QAngle(");
+					fix.edit.insert(document.uri, bodyRange.end, ")");
+				}
+			}
+			
+
 			fix.diagnostics = [diagnostic];
 
 			fixes.push(fix);
